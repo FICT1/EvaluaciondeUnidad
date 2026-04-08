ae80c94b12adf7e1acb2ac88a6aab6a1f971db35
vybdani <darochap@uamv.edu.ni>
2026-04-08 16:06:38 -0600

    Primeros avances

---
+++ Problema3/ProductosTienda/src/modelos/Main.java
@@ -0,0 +1,6 @@
+package modelos;
+
+public class Main {
+
+    public static void main(String[] args) {}
+}
---
+++ Problema3/ProductosTienda/src/modelos/Productos.java
@@ -0,0 +1,21 @@
+package modelos;
+
+public class Productos {
+
+    private String codigo;
+    private String nombre;
+    private float precio;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(String codigo, String nombre, float precio, int cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+
+}
